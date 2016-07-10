e32b8c2438e8d725da33caadfb77fd05acaeaf93
Zarrar Shehzad <czarrar@gmail.com>
2016-07-10 19:41:31 -0400

    fix to R includes

--- inst/include/bigextensions/bigex.h
+++ inst/include/bigextensions/bigex.h
@@ -4,14 +4,13 @@
 #include <iostream> // hack to make sure we are using the right "length" 
                     // function
 
-#include "bigmemory/BigMatrix.h"
-#include "bigmemory/MatrixAccessor.hpp"
-#include "bigmemory/bigmemoryDefines.h"
-#include "bigmemory/isna.hpp"
-
 #include <R.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
 
+#include "bigmemory/BigMatrix.h"
+#include "bigmemory/MatrixAccessor.hpp"
+#include "bigmemory/bigmemoryDefines.h"
+#include "bigmemory/isna.hpp"
 
 #endif //BIGEXTENSIONS_HEADER
